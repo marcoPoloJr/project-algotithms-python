e4f9e2321f8c6ab4671d2db39b23c255b61bc8ea
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-09 17:00:07 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+    except Exception:
+        return None
+
+    return counter
