b2503db8b56fe3c26db237cd6f01cf5ee4ab7cc0
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-09 21:06:24 -0300

    Req2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,12 @@
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-target_time = 5 
-print(permanence_period)
+""" a"""
 def study_schedule(permanence_period, target_time):
+""" a"""
     counter = 0
-
     try:
         for period in permanence_period:
             if period[0] <= target_time <= period[1]:
                 counter += 1
-    except Exception:
-        return print(None)
+    except ValueError:
+        return None
 
-    return print(counter)
-
-study_schedule(permanence_period,target_time)
+    return counter
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,24 +1,22 @@
+""" a"""
 import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    
+
     # Teste para key ímpar
     assert encrypt_message("message", 3) == "sem_egas"
-    # assert encrypt_message("trybe", 5) == "ebyrt"
-    
+
     # Teste para key par
     assert encrypt_message("message", 4) == "ega_ssem"
-    # assert encrypt_message("trybe", 2) == "eybtr"
-    
+
     # Teste para key inválido
     assert encrypt_message("message", -2) == "egassem"
-    # assert encrypt_message("trybe", 8) == "ebyrt"
-    
+
     # Teste para tipos incorretos de entrada
-    with pytest.raises(TypeError,match="tipo inválido para message"):
+    with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(5, 3)
-    
-    with pytest.raises(TypeError,match="tipo inválido para key"):
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("trybe", 3.9)
