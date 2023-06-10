7d7d76ffbee6bf70be4d447e0520e13204941161
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-10 17:03:31 -0300

    Req1 linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,25 @@
 """
-Linter
+Liner
 """
 
 
 def study_schedule(permanence_period, target_time):
     """
-    Linter
+    Liner
     """
     counter = 0
+
     try:
         for period in permanence_period:
             if period[0] <= target_time <= period[1]:
                 counter += 1
-    except ValueError:
+    except TypeError:
         return None
 
     return counter
+
+
+# test = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# target = 5
+
+# study_schedule(test, target)
